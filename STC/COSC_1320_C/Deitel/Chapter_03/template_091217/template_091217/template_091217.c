3a694b6a11b96b2798b932263f8d92a1870fa964
ge01 <gabriel.escobedo01@utrgv.edu>
2017-09-12 05:45:16 -0500

    template_091217

---
+++ STC/COSC_1320_C/Deitel/Chapter_03/template_091217/template_091217/template_091217.c
@@ -0,0 +1,17 @@
+// 
+// 
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+	// DECLATRATION		declare the program variables
+
+	// INITIALIZATION	initialize the program variables		
+
+	// PROCESSING		input data values and adjusts program variables
+								
+	// TERMINATION		calculate and print the final results
+	printf("%s", "Hello World!\n");
+		
+} // end function main
