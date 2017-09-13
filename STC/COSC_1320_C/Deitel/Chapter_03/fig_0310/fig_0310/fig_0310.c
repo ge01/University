522b6f856ec85ea6b54188c38493fd616158cd3e
ge01 <gabriel.escobedo01@utrgv.edu>
2017-09-13 12:10:11 -0500

    Analysis of examination results

---
+++ STC/COSC_1320_C/Deitel/Chapter_03/fig_0310/fig_0310/fig_0310.c
@@ -0,0 +1,43 @@
+// Fig. 3.10: fig_0310.c
+// Analysis of examination results.
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+	// INITIALIZATION		
+	// initialize variables in definitions
+	unsigned int passes = 0;			// number of passes
+	unsigned int failures = 0;			// number of failures
+	unsigned int student = 1;			// student counter
+	int result;							// one exam result
+
+	// PROCESSING		
+	// process 10 students using counter-controlled loop
+	while (student <= 10) {
+		
+		// prompt user for input and obtain value from user
+		printf("%s", "Enter result (1=pass, 2=fail): ");
+		scanf("%d", &result);
+
+		// if result 1, increment passes
+		if (result == 1) {
+			passes = passes + 1;
+		}								// end if 
+		else {							// otherwise, increment failures
+			failures = failures + 1;
+		}								// end else
+
+		student = student + 1;			// increment student counter
+	}									// end while
+
+	// TERMINATION
+	// display number of passes and failures
+	printf("Passed %u\n", passes);
+	printf("Failed %u\n", failures);
+
+	// if more than eight students passed, print "Bonus to instructor!"
+	if (passes > 8) {
+		puts("Bonus to instructor!");
+	}									// end if
+}										// end function main
