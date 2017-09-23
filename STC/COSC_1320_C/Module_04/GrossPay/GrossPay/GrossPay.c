6ee55be18abf80ed15ab1c765ac6eaa12eb48e6c
ge01 <gabriel.escobedo01@utrgv.edu>
2017-09-23 07:37:48 -0500

    detrmine the gross pay for each of several employees

--- STC/COSC_1320_C/Module_04/GrossPay/GrossPay/GrossPay.c
+++ STC/COSC_1320_C/Module_04/GrossPay/GrossPay/GrossPay.c
@@ -1,15 +1,20 @@
+//
+// September 24, 2017
+// This proga determins the gross pay for each of several employees.
+// The company pays “straight time” for the first 40 hours worked by 
+// each employee and pays “time-and-a-half” for all hours worked in 
+// excess of 40 hours. 
 #include <stdio.h>
 
 // function main begins program execution
 int main(void)
 {
 	// DECLARATION
-	int sentinel;
-	int hoursWorked;	// hours worked 
-	float hourlyRate;	// hourly rate
-	float grossPay;
-	float timeAndAHalfHours;
-	float timeAndAHalfRate;
+	int hoursWorked;           // hours worked and sentinel
+	float hourlyRate;          // hourly rate
+	float grossPay;            // gross pay
+	float timeAndAHalfHours;   // total hours after 40 hours
+	float timeAndAHalfRate;    // time and a half rate
 	
 	// PROCESSING
 	// get the # of hours worked
@@ -18,23 +23,22 @@ int main(void)
 	
 	while (hoursWorked != -1)
 	{
-		
+		// get the hourly rate
 		printf("%s", "Enter hourly rate of the worker ($00.00): ");
 		scanf("%f", &hourlyRate);
 
-		if (hoursWorked <= 40)
-		{
+		if (hoursWorked <= 40) {                   // calculate salary if <= 40 hours
 			grossPay = hourlyRate * hoursWorked;
-		}
-		else
-		{
+		} else {                                   // calculate salary if > 40 hours
 			timeAndAHalfHours = hoursWorked - 40;
 			timeAndAHalfRate = (hourlyRate / 2) * timeAndAHalfHours;
 			grossPay = (hourlyRate * hoursWorked) + timeAndAHalfRate;
 		}
 		
+		// print salary
 		printf("Salary is $%.2f\n\n", grossPay);
 
+		// get the # of hours worked
 		printf("%s", "Enter # of hours worked (-1 to end): ");
 		scanf("%d", &hoursWorked);	
 	}
