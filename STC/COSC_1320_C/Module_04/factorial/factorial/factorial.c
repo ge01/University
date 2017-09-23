8a0d4ceb092cb7ce74f311d7ab261aa5dde53723
ge01 <gabriel.escobedo01@utrgv.edu>
2017-09-23 05:13:49 -0500

    user inputs a nonnegative integer for factorial estimation

--- STC/COSC_1320_C/Module_04/factorial/factorial/factorial.c
+++ STC/COSC_1320_C/Module_04/factorial/factorial/factorial.c
@@ -6,37 +6,49 @@
 // function main begins program execution
 int main(void)
 {
-	// DECLARATION
-	int n;		// nonnegative integer
-	int count = 1;	// 
-	int factorial = 1; 
-	float eulersNumber;
+	// DECLARATION AND INITIALIZATION I
+	int n;				// holds a nonnegative integer
+	int count = 1;		// counter variable for while loops
+	int factorial = 1;	// holds factorial value
+	float eulersNumber;	// estimated value for euler's number (e)
 
-	// PROCESSING
-	printf("%s", "Enter a nonnegative integer and I will \n");
-	printf("%s", "print its factorial and \n");
-	printf("%s", "estimate the value of the mathematical constant theta \n");
-	printf("%s", "by using the formula: e=1+1/1!+1/2!+1/3! \n");
+	// PROCESSING I
+	printf("%s", "This part of the program reads a nonnegative integer \n");
+	printf("%s", "and computes and prints it's factorial. \n");
+	printf("%s", "Please enter a nonnegative integer: ");
 	scanf("%d", &n);
 
+	// loop n times set by user
 	while (count <= n) 
 	{
-		factorial *= count;
+		factorial *= count;		// compute factorial
+		++count;				// increment counter
+	}
 
-		if (count == 1) {
-			eulersNumber = (1.0 / factorial) + 1;
+	printf("factorial is %d \n", factorial);	// display result
+	
+	// INITIALIZATION II
+	n = 10;
+	count = 1;
+	factorial = 1;
+	
+	// PROCESSING II
+	while (count <= n)
+	{
+		factorial *= count;							// compute factorial
+
+		if (count == 1) {							// estimate euler's number
+			eulersNumber = (1.0 / factorial) + 1;	// 1 + 1/1! + ...
 		}
-		else
+		else										// ... + 1/10!
 		{
 			eulersNumber += (1.0 / factorial);
 		}
 		
-
-		++count;
+		++count;									// increment counter
 	}
 
-	printf("factorial is %d \n", factorial);
-	printf("e is %.8f\n", eulersNumber);
+	printf("e is %.5f\n", eulersNumber);			// display result
 		
 
 }
