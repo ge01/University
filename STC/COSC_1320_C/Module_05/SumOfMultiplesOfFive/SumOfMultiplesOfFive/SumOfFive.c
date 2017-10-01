9f6d6e271dd26d5383aa29e78414a550ca5378aa
ge01 <gabriel.escobedo01@utrgv.edu>
2017-09-30 20:37:03 -0500

    calculates and prints the sum of the multiple of 5's integers from 5 to 100

--- STC/COSC_1320_C/Module_05/SumOfMultiplesOfFive/SumOfMultiplesOfFive/SumOfFive.c
+++ STC/COSC_1320_C/Module_05/SumOfMultiplesOfFive/SumOfMultiplesOfFive/SumOfFive.c
@@ -1,16 +1,30 @@
+//
+// September 30, 2017
+// This program calculates and prints the sum of the 
+// multiple of 5's integers from 5 to 100.
 #include <stdio.h>
 
 int main(void)
 {
+	// INITIALIZATION
 	int sumNum = 0;
 	int count;
 
+	// header
+	printf("%s", "This program sums the displayed ");
+	printf("%s", "integers from 5 to 100. \n\n");
+
+	// PROCESSING
+	// calculate sum of the multiples of 5's integers
+	// from 5 to 100
 	for (count = 5; count <= 100; count += 5) {
 		sumNum += count;
 		printf("%d ", count);
-		//printf("%d ", sumNum);
 	}
-	printf("\n");
-	printf("Total is %d", sumNum);
-	printf("\n");
+
+	// TERMINATION
+	printf("\n\n");
+	printf("The sum of the displayed integers is ");
+	printf("%d.", sumNum);
+	printf("\n\n");
 }
