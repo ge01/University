8e455364ef0371943e6b2d98f21a5246ff5e3abe
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-04 09:21:13 -0500

    multiples working MEAN full stack app

--- STC/COSC_1320_C/Module_02/07_Multiples_MEAN/multiples/public/controllers/controller.js
+++ STC/COSC_1320_C/Module_02/07_Multiples_MEAN/multiples/public/controllers/controller.js
@@ -2,30 +2,49 @@ var myApp = angular.module('myApp', []);
  myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
    console.log("Hello World from controller");
 
-
+   // refresh the view
    var refresh = function() {
      $http.get('/multiples').success(function(response) {
        console.log("I got the data I requested");
+       // put the data into index.html
        $scope.multiples = response;
-       $scope.contact = "";
+       // clear input boxes after the refresh function is called
+       $scope.mult = "";
      });
    };
 
    refresh();
 
+   // function that recieves data from input boxes in the view
    $scope.addMult = function() {
+
+     // process if the first int is a multiple of the second
+     var isItMultiple = (parseInt($scope.mult.num2) % parseInt($scope.mult.num1));
+     console.log(isItMultiple);
+       if (isItMultiple == 0) {
+         $scope.mult.num3 = "YES";
+       } else {
+         $scope.mult.num3 = "NO";
+       }
+
+     // verify to the view data was received
      console.log($scope.mult);
+
+     // send data from input boxes to server
+     // .success takes response from the server as an argument
      $http.post('/multiples', $scope.mult).success(function(response) {
+       // Print response from the server/MongoDB
        console.log(response);
        refresh();
      });
    };
 
+   // remove function to delete data from DB
    $scope.remove = function(id) {
      console.log(id);
+     // Send delete request to the server
      $http.delete('/multiples/' + id).success(function(response) {
        refresh();
      });
    };
-
 }]);
--- STC/COSC_1320_C/Module_02/07_Multiples_MEAN/multiples/server.js
+++ STC/COSC_1320_C/Module_02/07_Multiples_MEAN/multiples/server.js
@@ -1,34 +1,49 @@
+// to be able to use the functions and commands contained within the Express module
 var express = require('express');
 var app = express();
+// require the mongojs module
 var mongojs = require('mongojs');
 var db = mongojs('multiples', ['multiples']);
+// allows the server to parse the data incoming from controller
 var bodyParser = require('body-parser');
 
-
+// express command to tell the server where to look for (static files) the
+// index.html file (view/template for app)
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
+// respond to the controllers request by sending data to the controller
 app.get('/multiples', function (req, res) {
   console.log("I received a GET request");
 
+  // have the server find the multiples database and collection
+  // docs - respond with the salary from the db
   db.multiples.find(function (err, docs) {
+    // make sure server received the data from the database
     console.log(docs);
+    // sends the data back to the controller
     res.json(docs);
   });
-
 });
 
+// listen for the post request from the controller
 app.post('/multiples', function(req, res) {
+  // print data it receives from the contrller to the command prompt
   console.log(req.body);
+  // insert data from the view to MongoDB
   db.multiples.insert(req.body, function(err, doc) {
+    // send back data to controller
     res.json(doc);
   });
 });
 
 app.delete('/multiples/:id', function (req, res) {
+  // get the value of the ID from the URL
   var id = req.params.id;
   console.log(id);
+  // delete salary from MongoDB
   db.multiples.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    // send back the data being removed to the controller
     res.json(doc);
   });
 });
