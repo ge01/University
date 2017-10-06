23482fc8bacb1b8ac661d943062e42734b51dbd3
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-05 22:26:25 -0500

    calculating sales

--- STC/COSC_1320_C/Module_06/01_CalculatingSales/01_CalculatingSales/CalculatingSales.c
+++ STC/COSC_1320_C/Module_06/01_CalculatingSales/01_CalculatingSales/CalculatingSales.c
@@ -2,31 +2,81 @@
 
 int main(void)
 {
-	int productNumber;
+	int holdProductNumber;
 	int quantitySold;
+	int randomVariable;
+	float oneDayPrice1 = 0;
+	float oneDayPrice2 = 0;
+	float oneDayPrice3 = 0;
+	float oneDayPrice4 = 0;
+	float oneDayPrice5 = 0;
+	float oneDayPrice6 = 0;
+	float weekPrice1 = 0;
+	float weekPrice2 = 0;
+	float weekPrice3 = 0;
+	float weekPrice4 = 0;
+	float weekPrice5 = 0;
+	float weekPrice6 = 0;
 
 	do {
 
 		printf("%s", "Enter the product number (-1 to end): ");
-		scanf("%d", &productNumber);
+		scanf("%d", &holdProductNumber);
 
-		if (productNumber != -1) {
-			
+		if (holdProductNumber != -1) {
+
 			printf("%s", "Enter the quantity sold for one day: ");
-			scanf("%d", &productNumber);
-
-			switch (productNumber)
-			{
-
-
-			default:
-				break;
-			}
+			scanf("%d", &quantitySold);
 		}
 
-		
+		switch (holdProductNumber)
+		{
+		case 1: 
+			printf("%s", "Data for product number 1 was saved\n\n");
+			oneDayPrice1 = quantitySold * 2.98;
+			weekPrice1 += oneDayPrice1;
+			break;
+		case 2:
+			printf("%s", "Data for product number 2 was saved\n\n");
+			oneDayPrice2 = quantitySold * 4.5;
+			weekPrice2 += oneDayPrice2;
+			break;
+		case 3:
+			printf("%s", "Data for product number 3 was saved\n\n");
+			oneDayPrice3 = quantitySold * 9.98;
+			weekPrice3 += oneDayPrice3;
+			break;
+		case 4:
+			printf("%s", "Data for product number 4 was saved\n\n");
+			oneDayPrice4 = quantitySold * 4.49;
+			weekPrice4 += oneDayPrice4;
+			break;
+		case 5:
+			printf("%s", "Data for product number 5 was saved\n\n");
+			oneDayPrice5 = quantitySold * 6.87;
+			weekPrice5 += oneDayPrice5;
+			break;
+		case 6:
+			printf("%s", "Data for product number 6 was saved\n\n");
+			oneDayPrice6 = quantitySold * 1.50;
+			weekPrice6 += oneDayPrice6;
+			break;
+		default:
+			if (holdProductNumber != -1) {
+				printf("%s", "Incorrect prodcut number entered.\n\n");
+			}
 
-	} while (productNumber != -1);
+			break;
+		}
 
-	printf("%s", "\nGoodbye\n\n");
+	} while (holdProductNumber != -1);
+
+	puts("\n\n\nProduct Number\tWeekly Retail Value");
+	puts("-----------------------------------");
+	printf("1\t\t$%.2f\n", weekPrice1);
+	printf("2\t\t$%.2f\n", weekPrice2);
+	printf("3\t\t$%.2f\n", weekPrice3);
+	printf("4\t\t$%.2f\n", weekPrice4);
+	printf("5\t\t$%.2f\n", weekPrice5);
+	printf("6\t\t$%.2f\n\n", weekPrice6);
 }
---
+++ STC/COSC_1320_C/Module_06/02_WeeklyPay/02_WeeklyPay/weeklyPay.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+int main(void)
+{
+	int employeeCode;
+
+
+	puts("EMPLOYEE CODE\tEMPLOYEE DESCRIPTION");
+	puts("------------------------------------");
+	puts("      1\t\tManagers");
+	puts("      2\t\tHourly Worker");
+	puts("      3\t\tCommission Worker");
+	puts("      4\t\tPieceworker");
+	puts("");
+	puts("Enter a code to compute the weekly pay");
+}
