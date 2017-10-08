d2589d3db40b4def80bba13d656e29f082a9df23
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-08 00:04:45 -0500

    weekly pay

--- STC/COSC_1320_C/Module_06/02_WeeklyPay/02_WeeklyPay/weeklyPay.c
+++ STC/COSC_1320_C/Module_06/02_WeeklyPay/02_WeeklyPay/weeklyPay.c
@@ -1,19 +1,27 @@
+//
+// October 7, 2017
+// This program computes the weekly pay for employees.  Each type of 
+// employee has its own pay code: Managers have paycode 1, hourly 
+// workers have code 2, commission workers have code 3 and pieceworkers 
+// have code 4. Use a switch to compute each employee’s pay based on 
+// that employee’s paycode.
 #include <stdio.h>
 
 int main(void)
 {
-	int employeeCode;
+	// DECLARATION
+	int employeeCode;          // to determine employee type
 	int hoursWorked;           // hours worked and sentinel
 	float hourlyRate;          // hourly rate
 	float grossPay;            // gross pay
 	float timeAndAHalfHours;   // total hours after 40 hours
 	float timeAndAHalfRate;    // time and a half rate
-	float weeklySales;
-	float fixedAmount; 
-	int itemsProduced;
+	float weeklySales;         // weekly sales for commission workers
+	float fixedAmount;         // for pieceworker cost per item
+	int itemsProduced;         // for pieceworker number of items produced
 
 
-
+	// HEADING
 	puts("EMPLOYEE CODE\tEMPLOYEE DESCRIPTION");
 	puts("------------------------------------");
 	puts("      1\t\tManager");
@@ -21,33 +29,38 @@ int main(void)
 	puts("      3\t\tCommission Worker");
 	puts("      4\t\tPieceworker");
 	
-	do {
-		printf("\nEnter a code to compute the weekly pay ");
+	// PROCESSING AND TERMINATION
+	do {// enter employee code
+		printf("\nEnter a employee code to compute the weekly pay: ");
 		scanf("%d", &employeeCode);		
 
+		// if employeee code is > 4 try again
 		if (employeeCode > 4) {
 			printf("\nEnter a number between 1 and 4:\n");
 		}
 	} while (employeeCode > 4);
 	
+	// determine which employee code was entered
 	switch (employeeCode)
 	{
-	case 1:
+	case 1: // MANAGERS message
 		puts("\nManagers are paid a fixed weekly salary.");
 		break;
-	case 2:
+	case 2: // HOURLY WORKER  calculation 		
 		// get the # of hours worked
 		printf("%s", "Enter # of hours worked: ");
 		scanf("%d", &hoursWorked);
 
 		// get the hourly rate
-		printf("%s", "Enter hourly rate of the worker ($00.00): ");
+		printf("%s", "Enter hourly rate of the worker (00.00): ");
 		scanf("%f", &hourlyRate);
 
-		if (hoursWorked <= 40) {                   // calculate salary if <= 40 hours
+		// calculate salary if <= 40 hours
+		if (hoursWorked <= 40) {                   
 			grossPay = hourlyRate * hoursWorked;
 		}
-		else {                                   // calculate salary if > 40 hours
+		// calculate salary if > 40 hours
+		else {                                   
 			timeAndAHalfHours = hoursWorked - 40;
 			timeAndAHalfRate = (hourlyRate / 2) * timeAndAHalfHours;
 			grossPay = (hourlyRate * hoursWorked) + timeAndAHalfRate;
@@ -56,28 +69,34 @@ int main(void)
 		// print salary
 		printf("Salary is $%.2f\n\n", grossPay);
 		break;
-	case 3:
-		printf("%s", "Enter gross weekly sales ($00.00): ");
+	case 3: // COMMISSION WORKER calculation
+		// get weekly sales
+		printf("%s", "Enter gross weekly sales (00.00): ");
 		scanf("%f", &weeklySales);
 
+		// calculate gross pay
 		grossPay = 250 + (.057 * weeklySales);
 
 		// print salary
 		printf("Salary is $%.2f\n\n", grossPay);
 		break;
-	case 4:
-		printf("%s", "Enter fixed amount of money ($00.00): ");
+	case 4: // Pieceworker calculation
+		// get fixed amount
+		printf("%s", "Enter fixed amount of money (00.00): ");
 		scanf("%f", &fixedAmount);
+
+		// get number of items produced
 		printf("%s", "Enter number of items produced (integer): ");
 		scanf("%d", &itemsProduced);
 
+		// calculate gross pay
 		grossPay = fixedAmount * itemsProduced;
 
 		// print salary
 		printf("Salary is $%.2f\n\n", grossPay);
 		break;
-	default:
-		puts("default");
+	default: // skip a line
+		puts("");
 		break;
 	}
 	
