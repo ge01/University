f663a0faac2e52811c68933887443e400d82da6d
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-08 02:55:44 -0500

    weekly pay added a second do while loop

--- STC/COSC_1320_C/Module_06/02_WeeklyPay/02_WeeklyPay/weeklyPay.c
+++ STC/COSC_1320_C/Module_06/02_WeeklyPay/02_WeeklyPay/weeklyPay.c
@@ -30,74 +30,78 @@ int main(void)
 	puts("      4\t\tPieceworker");
 	
 	// PROCESSING AND TERMINATION
-	do {// enter employee code
-		printf("\nEnter a employee code to compute the weekly pay: ");
-		scanf("%d", &employeeCode);		
+	do { // loop until user enters employee code -1
+		
+		do {// enter employee code, loop if user doesn't use code 1-4
+			printf("\nEnter a employee code to compute the weekly pay (-1 to end): ");
+			scanf("%d", &employeeCode);
 
-		// if employeee code is > 4 try again
-		if (employeeCode > 4) {
-			printf("\nEnter a number between 1 and 4:\n");
+			// if employeee code is > 4 try again
+			if (employeeCode > 4) {
+				printf("\nEnter a number between 1 and 4:\n");
+			}
+		} while (employeeCode > 4);
+
+		// determine which employee code was entered
+		switch (employeeCode)
+		{
+		case 1: // MANAGERS message
+			puts("\nManagers are paid a fixed weekly salary.");
+			break;
+		case 2: // HOURLY WORKER  calculation 		
+				// get the # of hours worked
+			printf("%s", "Enter # of hours worked: ");
+			scanf("%d", &hoursWorked);
+
+			// get the hourly rate
+			printf("%s", "Enter hourly rate of the worker (00.00): ");
+			scanf("%f", &hourlyRate);
+
+			// calculate salary if <= 40 hours
+			if (hoursWorked <= 40) {
+				grossPay = hourlyRate * hoursWorked;
+			}
+			// calculate salary if > 40 hours
+			else {
+				timeAndAHalfHours = hoursWorked - 40;
+				timeAndAHalfRate = (hourlyRate / 2) * timeAndAHalfHours;
+				grossPay = (hourlyRate * hoursWorked) + timeAndAHalfRate;
+			}
+
+			// print salary
+			printf("Salary is $%.2f\n", grossPay);
+			break;
+		case 3: // COMMISSION WORKER calculation
+				// get weekly sales
+			printf("%s", "Enter gross weekly sales (00.00): ");
+			scanf("%f", &weeklySales);
+
+			// calculate gross pay
+			grossPay = 250 + (.057 * weeklySales);
+
+			// print salary
+			printf("Salary is $%.2f\n", grossPay);
+			break;
+		case 4: // PIECEWORKER calculation
+				// get fixed amount
+			printf("%s", "Enter fixed amount of money (00.00): ");
+			scanf("%f", &fixedAmount);
+
+			// get number of items produced
+			printf("%s", "Enter number of items produced (integer): ");
+			scanf("%d", &itemsProduced);
+
+			// calculate gross pay
+			grossPay = fixedAmount * itemsProduced;
+
+			// print salary
+			printf("Salary is $%.2f\n", grossPay);
+			break;
+		default:// selected if user enters a number > 4
+			if (employeeCode != -1) {
+				printf("%s", "Incorrect prodcut number entered.\n\n");
+			}
+			break;
 		}
-	} while (employeeCode > 4);
-	
-	// determine which employee code was entered
-	switch (employeeCode)
-	{
-	case 1: // MANAGERS message
-		puts("\nManagers are paid a fixed weekly salary.");
-		break;
-	case 2: // HOURLY WORKER  calculation 		
-		// get the # of hours worked
-		printf("%s", "Enter # of hours worked: ");
-		scanf("%d", &hoursWorked);
-
-		// get the hourly rate
-		printf("%s", "Enter hourly rate of the worker (00.00): ");
-		scanf("%f", &hourlyRate);
-
-		// calculate salary if <= 40 hours
-		if (hoursWorked <= 40) {                   
-			grossPay = hourlyRate * hoursWorked;
-		}
-		// calculate salary if > 40 hours
-		else {                                   
-			timeAndAHalfHours = hoursWorked - 40;
-			timeAndAHalfRate = (hourlyRate / 2) * timeAndAHalfHours;
-			grossPay = (hourlyRate * hoursWorked) + timeAndAHalfRate;
-		}
-
-		// print salary
-		printf("Salary is $%.2f\n\n", grossPay);
-		break;
-	case 3: // COMMISSION WORKER calculation
-		// get weekly sales
-		printf("%s", "Enter gross weekly sales (00.00): ");
-		scanf("%f", &weeklySales);
-
-		// calculate gross pay
-		grossPay = 250 + (.057 * weeklySales);
-
-		// print salary
-		printf("Salary is $%.2f\n\n", grossPay);
-		break;
-	case 4: // Pieceworker calculation
-		// get fixed amount
-		printf("%s", "Enter fixed amount of money (00.00): ");
-		scanf("%f", &fixedAmount);
-
-		// get number of items produced
-		printf("%s", "Enter number of items produced (integer): ");
-		scanf("%d", &itemsProduced);
-
-		// calculate gross pay
-		grossPay = fixedAmount * itemsProduced;
-
-		// print salary
-		printf("Salary is $%.2f\n\n", grossPay);
-		break;
-	default: // skip a line
-		puts("");
-		break;
-	}
-	
+	} while (employeeCode != -1);	
 }
