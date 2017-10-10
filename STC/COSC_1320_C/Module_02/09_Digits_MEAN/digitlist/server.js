29a67d051dc8a4908b828581c04af8c7be9c7d06
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-10 04:15:20 -0500

    MEAN Stack separating digits in an integer (1/5)

---
+++ STC/COSC_1320_C/Module_02/09_Digits_MEAN/digitlist/server.js
@@ -0,0 +1,10 @@
+// To be able to use the functions and commands contained within the Express module
+var express = require('express');
+var app = express();
+
+// Express command to tell the server where to look for (static files) the
+// index.html file (view/template for app)
+app.use(express.static(__dirname + "/public"));
+
+app.listen(3000);
+console.log("Server running on port 3000");
