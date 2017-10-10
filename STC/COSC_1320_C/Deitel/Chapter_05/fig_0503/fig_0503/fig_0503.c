1e367b7c3aba3909677d394acf198f080ba4c304
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-09 21:43:03 -0500

    creating and using a programmer-defined function

---
+++ STC/COSC_1320_C/Deitel/Chapter_05/fig_0503/fig_0503/fig_0503.c
@@ -0,0 +1,24 @@
+// Fig. 5.3 fig_0503.c
+// Creating and using a programmer-defined function.
+#include <stdio.h>
+
+int square(int y); // function prototype
+
+// function main begins program execution
+int main(void)
+{
+	int x; // counter
+
+	// loop 10 times and calculate and output square of x each time
+	for (x = 1; x <= 10; ++x) {
+		printf("%d ", square(x)); // function call
+	} // end for
+
+	puts(" ");
+} // end main
+
+// square function definition returns the square of its parameter
+int square(int y) // y is a copy of the argument to the function
+{
+	return y * y; // returns the square of y as an int
+} // end function square
