28e71be77546848670883e764d0e81b26c863dcd
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-12 23:18:17 -0500

    shifted, scaled random integers produced by 1 + rand() % 6

---
+++ STC/COSC_1320_C/Deitel/Chapter_05/fig_0511/fig_0511/fig_0511.c
@@ -0,0 +1,22 @@
+// Fig. 5.11: fig_0511.c
+// Shifted, scaled random integers produced by 1 + rand() %6.
+#include <stdio.h>
+#include <stdlib.h>
+
+// function main begins program execution
+int main(void)
+{
+	unsigned int i; // counter
+
+					// loop 20 times
+	for (i = 1; i <= 20; ++i) {
+
+		// pick random number from 1 to 6 and output it 
+		printf("%10d", 1 + (rand() % 6));
+
+		// if counter is divisible by 5, begin new line of output
+		if (i % 5 == 0) {
+			puts("");
+		} // end if
+	} // end for 
+} // end main
