696c368a6ddd9c000dd2cfe0fa6fa24af5581dd8
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-21 23:22:36 -0500

    recursive exponentiation with comments

--- STC/COSC_1320_C/Module_08/03_RecursiveExponentiation/03_RecursiveExponentiation/RecursiveExponentiation.c
+++ STC/COSC_1320_C/Module_08/03_RecursiveExponentiation/03_RecursiveExponentiation/RecursiveExponentiation.c
@@ -1,3 +1,7 @@
+//
+// October 21, 2017
+// This program has a the recursive function power(base, exponent) that when
+// invoked returns base^exponent.  For example, power(3,4) = 3*3*3*3.
 #include<stdio.h>
 
 // function prototype
@@ -18,14 +22,16 @@ int main()
 	printf("Enter an integer for the exponent: ");
 	scanf("%d", &exponent);
 
-
+	// print base, exponent, and result
 	printf("%d^%d = %d\n", base, exponent, power(base, exponent));
 	return 0;
 }
 
-//
+// power function definition
 int power(int base, int exponent)
 {
-	if (exponent == 1) return base;
-	else return base*power(base, exponent - 1);
+	if (exponent == 1) 
+		return base;
+	else 
+		return base * power(base, exponent - 1);
 }
