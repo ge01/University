5e64be7ef936864a52e3b723d461da6fcb67fb56
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-05 01:51:52 -0500

    total sales 5 x 3 array

--- STC/COSC_1320_C/Module_10/02_TotalSales/02_TotalSales/TotalSales.c
+++ STC/COSC_1320_C/Module_10/02_TotalSales/02_TotalSales/TotalSales.c
@@ -6,17 +6,18 @@
 int main(void)
 {
 	// initialize array1, array2, array3
-	int array1[2][3] = { { 1, 2, 3 },{ 4, 5, 6 } };
+	int array1[5][3] = { { 1, 10, 100 },{ 2, 20, 200 },{ 3, 30, 300 },
+	                     { 4, 40, 400 },{ 5, 50, 500 } };
 
 	size_t i; // row counter
 	size_t j; // column counter
 
 			  // loop through rows
-	for (i = 0; i <= 1; ++i) {
+	for (i = 0; i < 5; ++i) {
 
 		// output column values
-		for (j = 0; j <= 2; ++j) {
-			printf("%d", array1[i][j]);
+		for (j = 0; j < 3; ++j) {
+			printf("%d ", array1[i][j]);
 		} // end inner for
 
 		printf("\n"); // start new line of output
