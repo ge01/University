972dac7f1fd95e5141d0c5716dba4a194901edb0
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-05 04:39:43 -0600

    total sales basic working array

--- STC/COSC_1320_C/Module_10/02_TotalSales/02_TotalSales/TotalSales.c
+++ STC/COSC_1320_C/Module_10/02_TotalSales/02_TotalSales/TotalSales.c
@@ -1,67 +1,68 @@
-// Fig. 6.22: fig06_22.c
-// Double-subscripted array manipulations.
+// 
+// 
 #include <stdio.h>
 #define PRODUCT 3
 #define SALESPERSON 4
 
 // function prototypes
-double average(const int setOfGrades[], size_t tests);
-void printArray(int grades[][SALESPERSON], size_t pupils, size_t tests);
+double total(const int setOfCost[], size_t person);
+void printArray(int costs[][SALESPERSON], size_t item, size_t agent);
 
 // function main begins program execution
 int main(void)
 {
-	size_t student; // student counter
+	size_t product; // product counter
 
-					// initialize student grades for thee students (rows)
-	int studentGrades[PRODUCT][SALESPERSON] =
+	// initialize product cost for three products (rows)
+	int productCost[PRODUCT][SALESPERSON] =
 	{ { 77, 68, 86, 73 },
 	{ 96, 87, 89, 78 },
 	{ 70, 90, 86, 81 } };
 
-	// output array studentGrades
+	// output array productCost
 	puts("The array is: ");
-	printArray(studentGrades, PRODUCT, SALESPERSON);
+	printArray(productCost, PRODUCT, SALESPERSON);
+	puts("");
 
-	// calculate average grade for each student
-	for (student = 0; student < PRODUCT; ++student) {
-		printf("The average grade for student %u is %.2f\n",
-			student, average(studentGrades[student], SALESPERSON));
+	// calculate the total cost for each product
+	for (product = 0; product < PRODUCT; ++product) {
+		printf("The total for product %u is %.2f\n",
+			product, total(productCost[product], SALESPERSON));
 	} // end for
 } // end main
 
-  // Determine the average grade for a particular student
-double average(const int setOfGrades[], size_t tests)
+// Determine the total for each product
+double total(const int setOfCost[], size_t person)
 {
-	size_t i; // exam counter
-	int total = 0; // sum of test grades
+	size_t i; // cost counter
+	int total = 0; // sum of product cost
 
-				   // total all grades for one student
-	for (i = 0; i < tests; ++i) {
-		total += setOfGrades[i];
+	// total cost for one product
+	for (i = 0; i < person; ++i) {
+		total += setOfCost[i];
 	} // end for
 
-	return (double)total / tests; // average
+	return total; // total
 } // end function average
 
   // Print the array
-void printArray(int grades[][SALESPERSON], size_t pupils, size_t tests)
+void printArray(int costs[][SALESPERSON], size_t item, size_t agent)
 {
-	size_t i; // student counter
-	size_t j; // exam counter
+	size_t i; // product counter
+	size_t j; // agent counter
 
-			  // output column heads
+	// output column heads
 	printf("%s", "                 [0]  [1]  [2]  [3]");
 
-	// output grades in tabular format
-	for (i = 0; i < pupils; ++i) {
+	// output costs in tabular format
+	for (i = 0; i < item; ++i) {
 
 		// output label for row
 		printf("\nProduct[%d] ", i);
 
-		// output grades for one student
-		for (j = 0; j < tests; ++j) {
-			printf("%-5d", grades[i][j]);
+		// output cost for one product
+		for (j = 0; j < agent; ++j) {
+			printf("%-5d", costs[i][j]);
 		} // end inner for
 	} // end outer for
 } // end function printArray
