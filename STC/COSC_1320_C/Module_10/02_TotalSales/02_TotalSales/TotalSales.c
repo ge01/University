e3c4a54a69dd7086d06a4412c26e97a7d36710bb
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-05 01:21:43 -0600

    total sales head out to 283

--- STC/COSC_1320_C/Module_10/02_TotalSales/02_TotalSales/TotalSales.c
+++ STC/COSC_1320_C/Module_10/02_TotalSales/02_TotalSales/TotalSales.c
@@ -6,16 +6,17 @@
 int main(void)
 {
 	// initialize array1
-	int array1[5][3] = {  1, 10, 100, 2, 20, 200, 3, 30, 300, 4, 40, 400, 5, 50, 500 };
+	int array1[2][15] = { {1, 10, 100, 2, 20, 200, 3, 30, 300, 4, 40, 400, 5, 50, 500},
+	                      { 1, 10, 100, 2, 20, 200, 3, 30, 300, 4, 40, 400, 5, 50, 500 } };
 
 	size_t i; // row counter
 	size_t j; // column counter
 
 			  // loop through rows
-	for (i = 0; i < 5; ++i) {
+	for (i = 0; i < 2; ++i) {
 
 		// output column values
-		for (j = 0; j < 3; ++j) {
+		for (j = 0; j < 15; ++j) {
 			printf("%d ", array1[i][j]);
 		} // end inner for
 
