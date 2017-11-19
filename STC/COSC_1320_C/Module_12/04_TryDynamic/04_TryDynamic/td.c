b5dd60d8ecd8ff3ef9353f00f8697836f33581ca
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-18 22:59:38 -0600

    pass array to getData function

--- STC/COSC_1320_C/Module_12/04_TryDynamic/04_TryDynamic/td.c
+++ STC/COSC_1320_C/Module_12/04_TryDynamic/04_TryDynamic/td.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 // Function prototypes
+void getData(int *testA, int scoresA);
 
 int main(void)
 {
@@ -16,14 +17,35 @@ int main(void)
 	//malloc() lets us reserve memory space in heap 
 	test = (int*)malloc(scores * sizeof(int));
 
-	printf_s("\nEnter the values of the array: ");
+	getData(test, scores);
+
+
+	//free(test);
+}
+
+// This function asks the user to input test scores that are stored
+// in an array
+void getData(int *testA, int scoresA)
+{
+	/*printf_s("\nEnter the values of the array: ");
 	int i;
-	for (i = 0; i < scores; i++)
-		scanf("%d", &test[i]);
+	for (i = 0; i < scoresA; i++)
+		scanf("%d", &testA[i]);
 
 	printf_s("\nThe values in the array are: \n");
-	for (i = 0; i < scores; i++)
-		printf("%d\t", test[i]);
+	for (i = 0; i < scoresA; i++)
+		printf("%d\t", testA[i]);*/
 
-	free(test);
+	printf_s("Enter each score.\n");
+	for (int i = 0; i < scoresA; ++i) {
+		do {
+			printf_s("Score # %d: ", (i));
+			scanf_s("%d", &testA[i]);
+
+			if (&testA < 0) {
+				printf_s("Score must not be negative.\n");
+				printf_s("Enter score again: ");
+			}
+		} while (&(testA) < 0);
+	}
 }
