3ae9d43ad532d24209d7d89e94109b65ff2490ed
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-19 15:38:40 -0600

    added comments

--- STC/COSC_1320_C/Module_12/04_TryDynamic/04_TryDynamic/td.c
+++ STC/COSC_1320_C/Module_12/04_TryDynamic/04_TryDynamic/td.c
@@ -1,3 +1,7 @@
+//
+// November 19, 2017
+// This program dynamically allocates an array large enough to hold a user-
+// defined number of test scores.
 #include <stdio.h>
 
 // Function prototypes
@@ -9,28 +13,22 @@ void displayData(int *testA, int scoresA, int Avg);
 int main(void)
 {
 	int *test;            // dynamically allocate an array
-	int average;       // hold average of test scores DOUBLE
+	int average;          // hold average of test scores DOUBLE
 	int scores;           // hold number of scores
 
 	// Get number of scores
-	printf_s("\nHow many scores do you have to average? ");
+	printf_s("How many scores do you have to average? ");
 	scanf_s("%d", &scores);
 
 	// Create array dynamically at program runtime
-	//malloc() lets us reserve memory space in heap 
+	//malloc() lets us reserve memory space
 	test = (int*)malloc(scores * sizeof(int));
 
+	// Call functions
 	getData(test, scores);
 	ascendingSort(test, scores);
-
-	average = getAverage(test, scores);
-	
+	average = getAverage(test, scores);	
 	displayData(test, scores, average);
-
-	// printf_s("%d", average);
-
-
-	//free(test);
 }
 
 // This function asks the user to input test scores that are stored
@@ -39,7 +37,9 @@ void getData(int *testA, int scoresA)
 {
 	printf_s("Enter each score.\n");
 
+	// Input test scores
 	for (int i = 0; i < scoresA; ++i) {
+		// Score must not be negative
 		do {
 			printf_s("Score # %d: ", (i));
 			scanf_s("%d", &testA[i]);
@@ -50,24 +50,22 @@ void getData(int *testA, int scoresA)
 			}
 		} while (&testA[i] < 0);
 	}
-
 	//printf_s("%d %d %d", &testA[0], testA[0], testA);
-
 }
 
 // This function sorts the Test array in ascedning order
 void ascendingSort(int *testA, int scoresA)
 {
-	unsigned int pass; // pass counter
-	size_t j; // comparison counter
-	unsigned int hold; // temporary locationused to swap elements
+	unsigned int pass;      // pass counter
+	size_t j;               // comparison counter
+	unsigned int hold;      // temporary locationused to swap elements
 
-					   // loop to control number of passes
+	// Loop to control number of passes
 	for (pass = 1; pass < scoresA; ++pass) {
-		// loop to control number of comparisons per pass
+		// Loop to control number of comparisons per pass
 		for (j = 0; j < scoresA - 1; ++j) {
 
-			// swap elements if out of order
+			// Swap elements if out of order
 			if (testA[j] > testA[j + 1]) {
 				hold = testA[j];
 				testA[j] = testA[j + 1];
@@ -75,21 +73,12 @@ void ascendingSort(int *testA, int scoresA)
 			} // end if
 		} // end inner for
 	} // end outer for
-
-	//printf("sorted array is...\n");
-	//for (int i = 0; i < scoresA; i++)
-	//{
-	//	printf("%d\n", testA[i]);
-	//}
-
 } 
-
-
-
 
 // This function calculates the average of the scores stored in an array
 int getAverage(int *testA, int scoresA)
 {
+	// Variables
 	int total = 0;
 	int i;
 
@@ -100,18 +89,19 @@ int getAverage(int *testA, int scoresA)
 	return total / scoresA;
 }
 
+// This function displays all data collected
 void displayData(int *testA, int scoresA, int Avg)
 {
+	// Variable
 	int i;
 
-	printf_s("\n Test scores\n");
-	printf_s("Number of scores: %d \n", scoresA);
+	printf_s("\nNumber of scores: %d \n", scoresA);
 	
-	printf("sorted array is...\n");
+	printf("The sorted array is... ");
 	for (int i = 0; i < scoresA; i++)
 	{
 		printf("%d ", testA[i]);
 	}
 
-	printf_s("Average score: %d", Avg);
+	printf_s("\nAverage score: %d \n\n", Avg);
 }
