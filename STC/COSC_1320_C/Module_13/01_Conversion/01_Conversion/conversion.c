c8cade1bba61e987263f51b9a6d375b5caa0037c
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-25 03:37:56 -0600

    added convertToLowercase

---
+++ STC/COSC_1320_C/Module_13/01_Conversion/01_Conversion/conversion.c
@@ -0,0 +1,38 @@
+// Fig. 7.10 fig_0710.c
+// Converting a string to uppercase using 
+// non-constant pointer to non-constant data.
+#include <stdio.h>
+#include <ctype.h>
+
+void convertToUppercase(char *sPtr); // prototype
+void convertToLower(char *sPtr); // prototype
+
+int main(void)
+{
+	char string[] = "cHaRaCters and $32.98"; // initialize char array
+
+	printf("The string before conversion is: %s", string);
+	convertToUppercase(string);
+	printf("\nThe string after conversion is: %s", string);
+	convertToLower(string);
+	printf("\nThe string after conversion is: %s", string);
+	puts("");
+} // end main
+
+// convert string to uppercase letters
+void convertToUppercase(char *sPtr)
+{
+	while (*sPtr != '\0') { // current character is not '\0'
+		*sPtr = toupper(*sPtr); // convert to uppercase
+		++sPtr; // make sPtr point to the next character
+	} // end while
+} // end function convertToUppercase
+
+// convert string to lowercase letters
+void convertToLower(char *sPtr)
+{
+	while (*sPtr != '\0') { // current character is not '\0'
+		*sPtr = tolower(*sPtr); // convert to lowercase
+		++sPtr; // make sPtr point to the next character
+	} // end while
+} // end function convertToLowercase
