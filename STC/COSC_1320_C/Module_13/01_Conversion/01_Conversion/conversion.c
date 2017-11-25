4c333f52f5343704792815ec8f70bfb435db86e2
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-25 05:54:26 -0600

    case conversion c-string

--- STC/COSC_1320_C/Module_13/01_Conversion/01_Conversion/conversion.c
+++ STC/COSC_1320_C/Module_13/01_Conversion/01_Conversion/conversion.c
@@ -1,51 +1,55 @@
-// Fig. 7.10 fig_0710.c
-// Converting a string to uppercase using 
-// non-constant pointer to non-constant data.
+//
+// November 25, 2017
+// Case Conversion C-String
 #include <stdio.h>
 #include <ctype.h>
 
-void convertToUppercase(char *sPtr); // prototype
-void convertToLower(char *sPtr); // prototype
-void printCharacters(const char *sPtr);
+void convertToUppercase(char *sPtr0); // prototype
+void convertToLower(char *sPtr1); // prototype
+void printCharacters(const char *sPtr2);
 
 int main(void)
 {
-	char string[] = "cHaRaCters and $32.98"; // initialize char array
+	char string0[] = "cHaRaCters and $32.98"; // initialize char array0
+	char string1[] = "GOOGLE.COM"; // initialize char array1
+	char string2[] = "COSC 1320"; // initialize char array2
 
-	printf("The string before conversion is: %s", string);
-	convertToUppercase(string);
-	printf("\nThe string after conversion is: %s", string);
-	convertToLower(string);
-	printf("\nThe string after conversion is: %s", string);
-	printCharacters(string);
-	printf("\nThe string after conversion is: %s", string);
+	printf("The string before conversion is: %s", string0);
+	convertToUppercase(string0);
+	printf("\nThe string after conversion is: %s\n", string0);
+
+	printf("The string before conversion is: %s", string1);
+	convertToLower(string1);
+	printf("\nThe string after conversion is: %s\n", string1);
+
+	printCharacters(string2);
 	puts("");
 } // end main
 
 // convert string to uppercase letters
-void convertToUppercase(char *sPtr)
+void convertToUppercase(char *sPtr0)
 {
-	while (*sPtr != '\0') { // current character is not '\0'
-		*sPtr = toupper(*sPtr); // convert to uppercase
-		++sPtr; // make sPtr point to the next character
+	while (*sPtr0 != '\0') { // current character is not '\0'
+		*sPtr0 = toupper(*sPtr0); // convert to uppercase
+		++sPtr0; // make sPtr0 point to the next character
 	} // end while
 } // end function convertToUppercase
 
 // convert string to lowercase letters
-void convertToLower(char *sPtr)
+void convertToLower(char *sPtr1)
 {
-	while (*sPtr != '\0') { // current character is not '\0'
-		*sPtr = tolower(*sPtr); // convert to lowercase
-		++sPtr; // make sPtr point to the next character
+	while (*sPtr1 != '\0') { // current character is not '\0'
+		*sPtr1 = tolower(*sPtr1); // convert to lowercase
+		++sPtr1; // make sPtr1 point to the next character
 	} // end while
 } // end function convertToLowercase
 
-// sPtr cannot modify the character to which it points,
-// i.e., sPtr is a "read-only" pointer
-void printCharacters(const char *sPtr)
+// sPtr2 cannot modify the character to which it points,
+// i.e., sPtr2 is a "read-only" pointer
+void printCharacters(const char *sPtr2)
 {
 	// loop through entire string
-	for (; *sPtr != '\0'; ++sPtr) { // no initialization
-		printf("%c", *sPtr);
+	for (; *sPtr2 != '\0'; ++sPtr2) { // no initialization
+		printf("%c", *sPtr2);
 	} // end for
 } // end function printCharacters
