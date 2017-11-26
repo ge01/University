fa39a36bd3ba32e3aafa3a35f8a1ac345c0b3fe1
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-26 03:18:00 -0600

    studentByReference()

--- STC/COSC_1320_C/Module_13/02_Reference/02_Reference/reference.c
+++ STC/COSC_1320_C/Module_13/02_Reference/02_Reference/reference.c
@@ -3,18 +3,24 @@
 
 #include <stdio.h>
 
-void cubeByReference(int *nPtr); // function prototype
+void cubeByReference(int *nPtr); 
+void studentByReference(char * studentinfo);
 
 int main(void)
 {
 	int number = 5; // initialize number
+	char studentString[] = "\nDon Quixote\n5487 Eagerest Dr.\nMentor, Ohio 52478";
 
 	printf("The original value of number is %d", number);
-
 	// pass address of number to cubeByReference
 	cubeByReference(&number);
-
 	printf("\nThe new value of number is %d\n", number);
+
+	puts("");
+	printf_s("The students info before fucntion call: %s\n", studentString);
+	studentByReference(studentString);
+	puts("");
+	printf_s("The students info after function call: %s\n\n", studentString);
 } // end main
 
   // calculate cube of *nPtr; actually modifies number in main
@@ -22,3 +28,20 @@ void cubeByReference(int *nPtr)
 {
 	*nPtr = *nPtr * *nPtr * *nPtr; // cube *nPtr
 } // end function cubeByReference
+
+void studentByReference(char * studentinfo)
+{
+	char studentUpdate[] = "\nJuan Haldudo\n1245 Jalisco Rd.\nParis, Texas 75268";
+	int j = 0;
+
+	for (int i = 0; *studentinfo != '\0'; ++i) {
+		*studentinfo = studentUpdate[i];
+		++studentinfo;
+	}
+
+	//while (*studentinfo != '\0') {
+	//	*studentinfo = studentUpdate[j];
+	//	++studentinfo;
+	//	++j;
+	//}
+}
