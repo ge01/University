620dca31c7c4b0328fecdeb1feb4cccbf20fb38e
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-26 04:33:43 -0600

    added comments to reference.c

--- STC/COSC_1320_C/Module_13/02_Reference/02_Reference/reference.c
+++ STC/COSC_1320_C/Module_13/02_Reference/02_Reference/reference.c
@@ -1,5 +1,8 @@
 // 
-// 
+// November 27, 2017
+// This program is a modification to the listing code in Fig. 7.7 in the
+// textbook to have one more function studentByReference(). In it, it updates
+// the student's name and address.
 
 #include <stdio.h>
 
@@ -8,40 +11,43 @@ void studentByReference(char * studentinfo);
 
 int main(void)
 {
-	int number = 5; // initialize number
+	// Initialization
+	int number = 5; 
 	char studentString[] = "\nDon Quixote\n5487 Eagerest Dr.\nMentor, Ohio 52478";
 
+	// Use cubeByReference
 	printf("The original value of number is %d", number);
-	// pass address of number to cubeByReference
+	
 	cubeByReference(&number);
+
 	printf("\nThe new value of number is %d\n", number);
 
+	// Use studentByReference
 	puts("");
 	printf_s("The students info before fucntion call: %s\n", studentString);
+
 	studentByReference(studentString);
+	
 	puts("");
 	printf_s("The students info after function call: %s\n\n", studentString);
 } // end main
 
-  // calculate cube of *nPtr; actually modifies number in main
+  // Calculate cube of *nPtr; actually modifies number in main
 void cubeByReference(int *nPtr)
 {
 	*nPtr = *nPtr * *nPtr * *nPtr; // cube *nPtr
 } // end function cubeByReference
 
+// Update student's name and address
 void studentByReference(char * studentinfo)
 {
+	// Initialization
 	char studentUpdate[] = "\nJuan Haldudo\n1245 Jalisco Rd.\nParis, Texas 75268";
 	int j = 0;
 
+	// Update students info one character at a time
 	for (int i = 0; *studentinfo != '\0'; ++i) {
 		*studentinfo = studentUpdate[i];
 		++studentinfo;
-	}
-
-	//while (*studentinfo != '\0') {
-	//	*studentinfo = studentUpdate[j];
-	//	++studentinfo;
-	//	++j;
-	//}
-}
+	} // end for
+} // end function studentByReference
