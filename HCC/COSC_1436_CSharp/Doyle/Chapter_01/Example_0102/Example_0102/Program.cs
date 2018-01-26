64e479a2e3a16f4b5cb959df1aee498a021893f1
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-25 19:13:43 -0600

    Multiline Comments

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_01/Example_0102/Example_0102/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* This is the beginning of a multiline (block) comment. It can
+             * go on for several lines or just be on a single line. No additinal
+             * symbols are needed after the geginning two characters. Notice there
+             * is no space placed between the two characters. To end the comment,
+             * use the following symbols. 
+             */
+        }
+    }
+}
