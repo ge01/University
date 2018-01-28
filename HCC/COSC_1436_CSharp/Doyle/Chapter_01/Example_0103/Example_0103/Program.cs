6663a2aaa6cd0df65fc009921ade17d237845b40
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-28 13:47:05 -0600

    Traditionally the first program written

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_01/Example_0103/Example_0103/Program.cs
@@ -0,0 +1,18 @@
+﻿// This is traditionally the first program written.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
