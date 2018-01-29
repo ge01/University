34e1fdaf31fab5e88b70c5096a7b47a72e18f2d7
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-28 20:17:17 -0600

    What is produced when you run the following application

---
+++ HCC/COSC_1436_CSharp/Chapter_01/Assignement_0101/Assignement_0101/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// using System.Console;  ERROR WHEN USING THIS LINE
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement_0101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Go ");
+            Console.Write("Forth ");
+            Console.Write("and DO ");
+            Console.Write("Awesome ");
+            Console.Write("Stuff!");
+        }
+    }
+}
