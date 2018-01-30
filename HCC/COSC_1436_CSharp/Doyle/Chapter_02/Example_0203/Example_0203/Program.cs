f2c1f3a939b6c4da8f1196e66680a82e069729d5
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-29 20:12:34 -0600

    Examples of integral variable declarations

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0203/Example_0203/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0203
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studentCount;       // number of students in the class
+            int ageOfStudent = 20;  // age - originally initialized to 20
+            int numberOfExams;      // number of exams
+            int coursesEnrolled;    // number of courses enrolled
+        }
+    }
+}
