6dd8d7af1bb31c1f47968aab6ce8bf2e6693e5a2
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-30 22:40:37 -0600

    Declaring double and float

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0205/Example_0205/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0205
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double extraPerson = 3.50;      // extraPerson originally set
+                                            // to 3.50
+            double averageScore = 70.0;     // averageScore originally set
+                                            // to 70.0
+            double priceOfTicket;           // cost of a movie ticket
+            double gradePointAverage;       // grade point average
+            float totalAmount = 23.57f;     // note that the f must be placed after
+                                            // the value for float types  
+        }
+    }
+}
