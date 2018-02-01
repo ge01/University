c6f0730d4c724273afe985649555ea0f2418413b
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-31 20:40:46 -0600

    Declaring Strings

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0208/Example_0208/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0208
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName;
+            string courseName = "Programming I";
+            string twoLines = "Line1\nLine2";       // newline escape sequence
+                                                    // character included
+        }
+    }
+}
