e6c1bcfa02f7cae3d331cd3590601ff27b84429a
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-31 20:22:49 -0600

    Using decimal type

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0206/Example_0206/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal endowmentAmount = 33897698.26M;
+            decimal deficit;
+        }
+    }
+}
