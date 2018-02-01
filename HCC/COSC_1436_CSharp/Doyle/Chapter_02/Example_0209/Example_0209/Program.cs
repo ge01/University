0378d2eebc8e9f42ac03690f87672125a45a4cac
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-31 20:47:39 -0600

    Making Data Constant

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0209/Example_0209/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double TAX_RATE = 0.0675;
+            const int SPEED = 70;
+            const char HIGHEST_GRADE = 'A';
+        }
+    }
+}
