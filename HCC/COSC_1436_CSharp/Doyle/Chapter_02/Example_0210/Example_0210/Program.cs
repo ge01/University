b4ce4eaf9a79e0373e489d7b71fa5af72891352b
ge01 <gabriel.escobedo01@utrgv.edu>
2018-01-31 20:57:03 -0600

    Declaring variables that can be used in assignment statements

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0210/Example_0210/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0210
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int     numberOfMinutes,
+                    count,
+                    minIntValue;
+            char    firstInitial,
+                    yearInSchool,
+                    punctuation,
+                    enterKey,
+                    lastChar;
+            double  accountBalance,
+                    weight;
+            decimal amountOwed,
+                    deficitValue;
+            bool    isFinished;
+            string  aSaying,
+                    fileLocation;
+        }
+    }
+}
