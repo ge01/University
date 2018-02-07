93fef21613c8fc6e3d48ba3a397c4561187a52b4
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-06 23:39:05 -0600

    Increment and decrement operations

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0211/Example_0211/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 100;
+            Console.WriteLine(num++);       // Displays 100
+            Console.WriteLine(num);         // Displays 101 after the post
+                                            // increment is performed on the 
+                                            // previos line
+            Console.WriteLine(++num);       // Displays 102
+        }
+    }
+}
