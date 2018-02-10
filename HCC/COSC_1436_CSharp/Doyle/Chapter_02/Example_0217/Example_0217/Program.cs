a5027fddddf55f32ae56ac49e1661255e2de8c90
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-09 22:49:53 -0600

    Casting makes a varialbe temporarily behave as if it is a different type

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0217/Example_0217/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0217
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 0,
+                anotherNumber = 0;
+            double value2 = 100,
+                anotherDouble = 100;
+            value1 = (int)value2;
+            value2 = (double)anotherNumber;
+        }
+    }
+}
