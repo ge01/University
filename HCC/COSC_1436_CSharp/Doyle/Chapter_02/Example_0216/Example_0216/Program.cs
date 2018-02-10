cf3101c4683e4f13a07c131d691d2303e9164ee0
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-09 22:04:17 -0600

    Implicit type conversion is made possible by changing the value of the literal

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0216/Example_0216/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0216
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Result of assignment statement is 89.666666
+            int exam1 = 86,
+                exam2 = 92,
+                exam3 = 91;
+            double examAverage;
+            examAverage = (exam1 + exam2 + exam3) / 3.0;
+        }
+    }
+}
