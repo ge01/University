97648b0e70ca2b2fe1ef71146880f0ff215d3bb7
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-09 21:48:32 -0600

    Double data type but integers right of the assignment operator

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0214/Example_0214/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0214
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Result is 3.0 not 3.333333
+            double answer;
+            answer = 10 / 3;
+        }
+    }
+}
