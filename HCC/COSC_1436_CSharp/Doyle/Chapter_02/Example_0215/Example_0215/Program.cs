48de68ee1679ebaeeb4e735aa1b1b6d0b4021770
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-09 21:55:02 -0600

    Cannot implicitly convert type 'double' to 'int'

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0215/Example_0215/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 440,
+                anotherNumber = 70;
+            double value2 = 100.60,
+                anotherDouble = 100.999;
+            value1 = value2;                // syntax error as shown in Figure 2-14
+            value2 = anotherNumber;         // 100.60 cannot be stored in value1
+        }
+    }
+}
