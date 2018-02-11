b065bddd08e51b42fdf8b63f156f1fe47ae8822f
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-10 22:05:58 -0600

    Using the currency format specifier to display a number with commas, decimal, and a dollar symbol

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Example_0218/Example_0218/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price = 1089.30;
+            Console.WriteLine("The price is {0:C}.", price);
+            // Display the price is $1,089.30.
+        }
+    }
+}
