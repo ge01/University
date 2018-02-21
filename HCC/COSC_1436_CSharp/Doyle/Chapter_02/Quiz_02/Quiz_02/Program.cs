bf6277093f9850d12eefb51373c17781c640e04c
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-21 00:09:48 -0600

    When the expression evalutes to true, the true statement are executed

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_02/Quiz_02/Quiz_02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ans = 0, v1 = 5, v2 = 8;
+            ans = v2 % v1++;
+
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_05/Example_0510/Example_0510/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0510
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (hoursWorked > 40)
+            {
+                payAmount = (hoursWorked - 40) * payRate * 1.5 + payRate * 40;
+                Console.WriteLine("You worked {0} hours overtime.",
+                                    hoursWorked - 40);
+            }
+            else
+                payAmount = hoursWorked * payRate;
+            Console.WriteLine("I am displayed, whether the expression "
+                                + "evalutes true or false");
+        }
+    }
+}
