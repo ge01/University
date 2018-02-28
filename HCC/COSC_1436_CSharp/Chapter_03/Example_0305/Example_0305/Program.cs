5f217f0b1024ca6f7f62739c4812ad1c11e3f7cc
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-27 20:06:21 -0600

    The WriteLine() Method

---
+++ HCC/COSC_1436_CSharp/Chapter_03/Example_0305/Example_0305/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0305
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(45);
+            Console.WriteLine("An apple a day"
+                                + " keeps the doctor away.");
+            Console.WriteLine(67.28 + 10000);
+            Console.WriteLine(true);
+            Console.WriteLine("Score on the next exam: {0}", 100);
+        }
+    }
+}
