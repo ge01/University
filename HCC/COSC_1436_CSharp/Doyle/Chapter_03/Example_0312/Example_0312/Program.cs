1e79400a3c603c05c00de4d96210ce805db7fb80
ge01 <gabriel.escobedo01@utrgv.edu>
2018-03-01 03:13:19 -0600

    Moved chapter 3 to Doyle

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_03/Example_0312/Example_0312/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0312
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aValue = 200;
+            int bValue = 896;
+            int result;
+            result = Math.Max(aValue, bValue);
+            // result = 896
+            result += bValue * Math.Max(aValue, bValue) - aValue;
+            // result = 896 + (896 * 896 - 200)
+            // result = 803512
+            Console.WriteLine("Largest value between {0} "
+                    + "and {1} is {2}", aValue, bValue,
+                    Math.Max(aValue, bValue));
+        }
+    }
+}
