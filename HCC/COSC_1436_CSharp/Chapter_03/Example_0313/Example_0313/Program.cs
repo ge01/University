73024df4b53f36d03b9f1a5b743026b13f731536
ge01 <gabriel.escobedo01@utrgv.edu>
2018-03-01 21:16:29 -0600

    Void Methods

---
+++ HCC/COSC_1436_CSharp/Chapter_03/Example_0313/Example_0313/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0313
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayInstructions();
+        }
+
+        public static void DisplayInstructions()
+        {
+            Console.WriteLine("This program will determin how "
+                                + "much carpet to purchase.");
+            Console.WriteLine();
+            Console.WriteLine("You will be asked to enter the "
+                                + "size of the room and ");
+            Console.WriteLine("the price of the carpet, "
+                                + "in price per square yards.");
+            Console.WriteLine();
+        }
+    }
+}
