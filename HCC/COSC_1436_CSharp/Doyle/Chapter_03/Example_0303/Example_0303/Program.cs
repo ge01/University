2e5e13842f8c83ea74a82b58fe861c315d8bb1af
ge01 <gabriel.escobedo01@utrgv.edu>
2018-03-02 19:57:43 -0600

    Calculate miles per gallon

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_03/Example_0303/Example_0303/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Miles per gallon = {0:N2}", CalculateMilesPerGallon(289, 12.2));
+        }
+
+        public static double CalculateMilesPerGallon(int milesTraveld, double gallonsUsed)
+        {
+            return milesTraveld / gallonsUsed;
+        }
+    }
+}
