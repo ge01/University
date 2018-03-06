67cf61b08ae4265726f816f0bd4c3db41e76c7bc
ge01 <gabriel.escobedo01@utrgv.edu>
2018-03-05 20:52:15 -0600

    Demonstrates use of a loop to add 10 integers

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_06/Example_0601/Example_0601/Program.cs
@@ -0,0 +1,30 @@
+﻿/* SummedValues.cs
+ * Demonstrates use of a loop to add 10
+ * integers. Displays the total after
+ * the loop body is completed.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int number = 1;
+            while (number < 11)
+            {
+                sum = sum + number;
+                number++;
+            }
+            Console.WriteLine("Sum of values "
+                                + "1 through 10"
+                                + " is " + sum);
+        }
+    }
+}
