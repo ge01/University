22e5a399071da8a1b600f4e6d62f58a215526442
ge01 <gabriel.escobedo01@utrgv.edu>
2018-03-14 20:39:42 -0500

    Display age one year from now

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_03/Example_0309/Example_0309/Program.cs
@@ -0,0 +1,28 @@
+﻿/**************************************
+ * AgeIncrementer.cs
+ * Displays age one year from now.
+ *************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0309
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string aValue;
+
+            Console.Write("Enter your are: ");
+            aValue = Console.ReadLine();
+            age = int.Parse(aValue);
+            Console.WriteLine("Your age next year"
+                        + " will be {0}", ++age);
+            Console.Read();
+        }
+    }
+}
