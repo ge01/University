a54ea1734d028199d893383fd1ee4ada281c7531
ge01 <gabriel.escobedo01@utrgv.edu>
2018-03-21 18:41:55 -0500

    Using double.Parse(inputStringValue) & Math.Pow(aValue, 2)

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_03/Example_0310/Example_0310/Program.cs
@@ -0,0 +1,32 @@
+﻿/*********************************************
+ * SquareInput.cs
+ * Allows an integer value to be input.
+ * Computes and displays the square of the 
+ * value inputted.
+ *********************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0310
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStringValue;
+            double aValue;
+            double result;
+
+            Console.Write("Enter a value to be "
+                            + "squared: ");
+            inputStringValue = Console.ReadLine();
+            aValue = double.Parse(inputStringValue);
+            result = Math.Pow(aValue, 2);
+            Console.WriteLine("{0} squared is {1}",
+                                aValue, result); 
+        }
+    }
+}
