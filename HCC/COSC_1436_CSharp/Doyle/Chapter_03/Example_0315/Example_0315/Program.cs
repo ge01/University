c296a251a46efd137e1864178af2e683f3f7987a
ge01 <gabriel.escobedo01@utrgv.edu>
2018-03-26 18:58:19 -0500

    public static double GetLength()

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_03/Example_0315/Example_0315/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_0315
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetLength();
+        }
+
+        public static double GetLength()
+        {
+            string inputValue;
+            int feet,
+                inches;
+
+            Console.Write("Enter the Length in feet: ");
+            inputValue = Console.ReadLine();
+            feet = int.Parse(inputValue);
+            Console.Write("Enter the Length in inches: ");
+            inputValue = Console.ReadLine();
+            inches = int.Parse(inputValue);
+            return (feet + (double)inches / 12);
+        }
+    }
+}
