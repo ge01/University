05e7a90c463a2c97fbaa6ccefaa8d95b07f1e82e
ge01 <gabriel.escobedo01@utrgv.edu>
2018-04-15 13:00:56 -0500

    4-13 C# Employee Class - Intro to Classes and Objects

---
+++ HCC/COSC_1436_CSharp/Chapter_04/EmployeeAssignment/EmployeeAssignment/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAssignment
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private decimal _salary;
+
+        // Constructor
+        public Employee(string firstName, string lastName, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+        }
+
+        public decimal Salary
+        {
+            get { return _salary; }
+            set
+            {
+                if (value >= 0)
+                    _salary = value;
+            }
+        }
+    }
+}
