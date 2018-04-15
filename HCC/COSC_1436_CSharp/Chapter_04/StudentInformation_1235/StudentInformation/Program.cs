9476c437a5f2b15361371a6d0182f678ca22a85d
ge01 <gabriel.escobedo01@utrgv.edu>
2018-04-15 12:35:56 -0500

    Student Info 1235

---
+++ HCC/COSC_1436_CSharp/Chapter_04/StudentInformation_1235/StudentInformation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformation
+{
+    class StudentInformationApp
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo Gabe = new StudentInfo();
+            Console.WriteLine("Name: " + Gabe.FirstName + " " + Gabe.LastName);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HCC/COSC_1436_CSharp/Chapter_04/StudentInformation_1235/StudentInformation/StudentInfo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformation
+{
+    class StudentInfo
+    {
+
+        private int studentNumber;
+        private string firstName;
+        private string lastName;
+        private string studentClass;
+        private string studentMajor;
+
+        public int StudentNumber
+        {
+            get
+            {
+                return studentNumber;
+            }
+        }
+        public void setFirstName(String newFirstName)
+        {
+            firstName = newFirstName;
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+        }
+        public void setLastName(String newLastName)
+        {
+            lastName = newLastName;
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+        }
+        public string StudentClass
+        {
+            get
+            {
+                return studentClass;
+            }
+        }
+        public string StudentMajor
+        {
+            get
+            {
+                return studentMajor;
+            }
+        }
+        //Default Constructor
+        public StudentInfo()
+        {
+        }
+        //Constructor 1
+        public StudentInfo(int number, string first, string last, string classification, string major)
+        {
+            studentNumber = number;
+            firstName = first;
+            lastName = last;
+            studentClass = classification;
+            studentMajor = major;
+        }
+        //Constructor 2
+        public StudentInfo(string first, string last)
+        {
+            firstName = first;
+            lastName = last;
+        }
+
+    }
+}
