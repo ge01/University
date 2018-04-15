2f93d4d284ed6ec7d27530f09d5329a9d49c8a51
ge01 <gabriel.escobedo01@utrgv.edu>
2018-04-15 12:17:46 -0500

    Classes - Beginner Tutorial

---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_04/FirstConsoleProject/FirstConsoleProject/Animal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleProject
+{
+    class Animal
+    {
+
+
+        // CLASS VARIABLES
+        
+        //public string name = "Spotty";
+        //public int age = 6;
+        //public float happiness = 0.5f;
+
+        public static int Count = 0;
+
+        public string name;
+        public int age;
+        public float happiness;
+
+        // CLASS CONTRUCTORS
+        public Animal()
+        {
+            name = "Spotty";
+            age = 6;
+            happiness = 0.5f;
+
+            Count++;
+        }
+
+        public Animal(string _name, int _age, float _happiness)
+        {
+            name = _name;
+            age = _age;
+            happiness = _happiness;
+
+            Count++;
+        }
+
+        // Access modifiers
+            // Types: private, public, static
+
+        // CLASS METHODS
+        public void Print()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Happiness: " + happiness);
+        }
+
+
+        
+    }
+}
---
+++ HCC/COSC_1436_CSharp/Doyle/Chapter_04/FirstConsoleProject/FirstConsoleProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleProject
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            // Instaniate a class
+            Animal dog = new Animal();
+            Console.WriteLine(dog.name);
+            dog.name = "Test";
+            Console.WriteLine(dog.name);
+
+            dog.Print();
+
+            Console.WriteLine();
+
+            // Animal.Count += 2;
+
+            Animal cat = new Animal("Mr. Beans", 10, 0.8f);
+            cat.Print();
+
+            Console.WriteLine();
+            Console.WriteLine("Num of Animals: " + Animal.Count);
+
+            //Console.WriteLine(Animal.Count);
+
+        }
+    }
+}
