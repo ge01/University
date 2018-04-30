2301e2a6e0c47acdc4af3c9c7d019ef15c63c23b
ge01 <gabriel.escobedo01@utrgv.edu>
2018-04-29 20:54:30 -0500

    Average rainfall for the year

--- HCC/COSC_1436_CSharp/Chapter_07/Assignment_0701/Assignment_0701/Program.cs
+++ HCC/COSC_1436_CSharp/Chapter_07/Assignment_0701/Assignment_0701/Program.cs
@@ -14,30 +14,35 @@ namespace Assignment_0701
             double avg;
             double distance;
             string inValue;
-            int[] score = new int[10];
+            int[] rainfallValue = new int[12];
+            string[] month = new string[12] { "Jan", "Feb", "Mar", "Apr",
+                "May", "Jun", "Jul", "Aug", "Sep",
+                "Oct", "Nov", "Dec"};
 
             // Values are entered
-            for (int i = 0; i < score.Length; i++)
+            for (int i = 0; i < rainfallValue.Length; i++)
             {
-                Console.Write("Enter Score{0}: ", i + 1);
+                Console.Write("Enter Rainfall integer in inches for month {0}: ", i + 1);
                 inValue = Console.ReadLine();
-                score[i] = Convert.ToInt32(inValue);
+                rainfallValue[i] = Convert.ToInt32(inValue);
             }
 
             // Values are summed.
-            for (int i = 0; i < score.Length; i++)
+            for (int i = 0; i < rainfallValue.Length; i++)
             {
-                total += score[i];
+                total += rainfallValue[i];
             }
 
-            avg = total / score.Length;
+            avg = total / rainfallValue.Length;
             Console.WriteLine();
-            Console.WriteLine("Score\tDist. from Avg.");
-            for (int i = 0; i < score.Length; i++)
+            Console.WriteLine("Month\tInches\tDist. from Avg.");
+            for (int i = 0; i < rainfallValue.Length; i++)
             {
-                distance = Math.Abs((avg - score[i]));
-                Console.WriteLine("{0}\t\t{1}", score[i], distance);
+                distance = Math.Abs((avg - rainfallValue[i]));
+                Console.WriteLine("{0}\t{1}\t{2}", month[i], rainfallValue[i], distance);
             }
+            Console.WriteLine();
+            Console.WriteLine("The average rainfall for the year is {0} inches.", avg);
         }
     }
 }
