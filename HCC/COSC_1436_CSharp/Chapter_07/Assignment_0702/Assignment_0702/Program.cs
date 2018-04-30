aa5a994fa25309fc1c4d2d7a6f38ca2fee3e5419
ge01 <gabriel.escobedo01@utrgv.edu>
2018-04-29 21:31:51 -0500

    Sort names in an array

---
+++ HCC/COSC_1436_CSharp/Chapter_07/Assignment_0702/Assignment_0702/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_0702
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fullName = new string[100];
+            string inValue;
+            int nameCount = 0;
+
+            Console.Write("-1 to exit, Enter name {0}: ", nameCount + 1);
+            inValue = Console.ReadLine();
+
+            while (inValue != "-1")
+            {
+                fullName[nameCount] = inValue;
+                ++nameCount;
+                Console.Write("-1 to exit, Enter name {0}: ", nameCount + 1);
+                inValue = Console.ReadLine();
+            }
+
+            Array.Sort(fullName);
+            
+            for (int i = 0; i < fullName.Length; i++)
+            {
+                Console.WriteLine(fullName[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
