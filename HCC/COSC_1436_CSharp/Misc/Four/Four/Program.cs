268c775e8a389df37b96c7f735a60da18093ef75
ge01 <gabriel.escobedo01@utrgv.edu>
2018-05-05 07:29:12 -0500

    four

---
+++ HCC/COSC_1436_CSharp/Misc/Four/Four/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] values = new double[5] { 5.0, 10.0, 15.0, 20.0, 25.0};
+            double max = 4;
+
+            if (values.Length > max)
+            {
+                for (int count = 0; count < values.Length; count++)
+                {
+                    Console.WriteLine(count);
+                }
+            }
+
+
+        }
+    }
+}
