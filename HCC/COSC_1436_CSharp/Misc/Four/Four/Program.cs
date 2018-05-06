8778ced2c2a7ff0a4f8a44a9b47b783857b727b7
ge01 <gabriel.escobedo01@utrgv.edu>
2018-05-06 16:27:18 -0500

    Six

--- HCC/COSC_1436_CSharp/Misc/Four/Four/Program.cs
+++ HCC/COSC_1436_CSharp/Misc/Four/Four/Program.cs
@@ -10,7 +10,7 @@ namespace Four
     {
         static void Main(string[] args)
         {
-            double[] values = new double[5] { 5.0, 10.0, 15.0, 20.0, 25.0};
+            double[] values = new double[5] { 5.0, 10.0, 15.0, 20.0, 25.0 };
             double max = 4;
 
             if (values.Length > max)
@@ -21,7 +21,43 @@ namespace Four
                 }
             }
 
+            Console.WriteLine();
 
+            const double PI = 3.1416;
+            double volume = 0;
+            double radius = 5;
+            double area;
+
+            // Calculate the area of a sphere
+            area = 4 * PI * Math.Pow(radius, 2);
+
+            // Display the area of a sphere
+            Console.WriteLine(area);
+
+            Console.WriteLine();
+
+            double number;
+            int a = 0, b = 0, c = 0;
+
+            Console.Write("Enter number: ");
+            number = double.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                a--;
+            }
+            else if (number < 55)
+            {
+                b--;
+            }
+            else
+            {
+                c--;
+            }
+
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+            Console.WriteLine("c = {0}", c);
         }
     }
 }
