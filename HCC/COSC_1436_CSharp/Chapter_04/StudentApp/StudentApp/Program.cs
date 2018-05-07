b45c1f2f7c367f99ec583c36147d63d56eacff56
ge01 <gabriel.escobedo01@utrgv.edu>
2018-05-07 02:39:41 -0500

    Student App

---
+++ HCC/COSC_1436_CSharp/Chapter_04/StudentApp/StudentApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    class StudentApp
+    {
+        static void Main(string[] args)
+        {
+            Student firstStudentObject = new Student();
+            firstStudentObject.StudentName = AskForStudentName();
+            firstStudentObject.StudentNumber = AskForStudentNumber();
+            firstStudentObject.Major = AskForMajor(firstStudentObject.StudentName);
+            firstStudentObject.Score1 = AskForExamScore(1);
+            firstStudentObject.Score1 = AskForExamScore(2);
+            firstStudentObject.Score1 = AskForExamScore(3);
+
+            Console.Clear();
+            Console.WriteLine("First Student");
+            Console.WriteLine(firstStudentObject.ToString());
+
+            Student secondStudentObject = new Student(2345);
+            secondStudentObject.Score1 = 95;
+            secondStudentObject.Score2 = 62;
+            secondStudentObject.Score3 = 87;
+            Console.WriteLine("\n\nSecond Student");
+            Console.WriteLine("Student Number: " + secondStudentObject.StudentNumber +
+                                "\nAverage: {0:f1}", secondStudentObject.CalculateAverage());
+
+            Student thirdStudentObject = new Student(5432, "Randolph Wonder");
+            thirdStudentObject.Major = "Math";
+            thirdStudentObject.Score1 = 95;
+            thirdStudentObject.Score2 = 87;
+            thirdStudentObject.Score3 = 72;
+            Console.WriteLine("\n\nThird Student");
+            Console.WriteLine(thirdStudentObject);
+
+            Student aStudentObject = new Student(1234, "Maria Smith",
+                                                        97, 75, 87, "CS");
+
+            Console.WriteLine("\n\nLast Student");
+            Console.WriteLine("Student Name: " + aStudentObject.StudentName +
+                "\nStudent Number: " + aStudentObject.StudentNumber +
+                "\nMajor: " + aStudentObject.Major +
+                "\nExam Score 1: " + aStudentObject.Score1 +
+                "\nExam Score 2: " + aStudentObject.Score2 +
+                "\nExam Score 3: " + aStudentObject.Score3 +
+                "\nExam Average: " +
+                aStudentObject.CalculateAverage().ToString("f1"));
+            Console.ReadKey();
+        }
+
+        public static int AskForExamScore(int whichOne)
+        {
+            string inValue;
+            int aScore;
+            Console.Write("Enter a value for Score {0}: ", whichOne);
+            inValue = Console.ReadLine();
+            aScore = int.Parse(inValue);
+            return aScore;
+        }
+
+        public static string AskForStudentName()
+        {
+            string inValue;
+            Console.Write("Enter Student Name: ");
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+
+        public static string AskForMajor(string name)
+        {
+            string inValue;
+            Console.Write("Enter {0}\'s Major: ", name);
+            inValue = Console.ReadLine();
+            return inValue;
+        }
+
+        public static int AskForStudentNumber()
+        {
+            string inValue;
+            Console.Write("Enter Student Number: ");
+            inValue = Console.ReadLine();
+            return (int.Parse(inValue));
+        }
+    }
+}
---
+++ HCC/COSC_1436_CSharp/Chapter_04/StudentApp/StudentApp/Student.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    class Student
+    {
+        // Data members, data fields, or characteristics
+        private int studentNumber;
+        private string studentName;
+        private int score1;
+        private int score2;
+        private int score3;
+        private string major;
+
+        // default constructor
+        public Student()
+        {
+
+        }
+
+        // constructor with one argument
+        public Student(int sID)
+        {
+            studentNumber = sID;
+        }
+
+        // constructor with two arguments
+        public Student(int sID, string name)
+        {
+            studentNumber = sID;
+            studentName = name;
+        }
+
+        // constructor with six arguments
+        public Student(int sID, string name,
+            int s1, int s2, int s3, string maj)
+        {
+            studentNumber = sID;
+            studentName = name;
+            score1 = s1;
+            score2 = s2;
+            score3 = s3;
+            major = maj;
+        }
+
+        // Properties
+        public string StudentName
+        {
+            get
+            {
+                return studentName;
+            }
+            set
+            {
+                studentName = value;
+            }
+        }
+
+        public int StudentNumber
+        {
+            get
+            {
+                return studentNumber;
+            }
+            set
+            {
+                studentNumber = value;
+            }
+        }
+
+        public string Major
+        {
+            get
+            {
+                return major;
+            }
+            set
+            {
+                major = value;
+            }
+        }
+
+        public int Score1
+        {
+            get
+            {
+                return score1;
+            }
+            set
+            {
+                score1 = value;
+            }
+        }
+
+        public int Score2
+        {
+            get
+            {
+                return Score2;
+            }
+            set
+            {
+                Score2 = value;
+            }
+        }
+
+        public int Score3
+        {
+            get
+            {
+                return Score3;
+            }
+            set
+            {
+                Score3 = value;
+            }
+        }
+
+        public double CalculateAverage()
+        {
+            return (score1 + score2 + score3) / 3.0;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " +
+                studentName +
+                "\nMajor: " +
+                major +
+                "\nScore Average: " +
+                CalculateAverage().ToString("f2");
+        }
+    }
+}
