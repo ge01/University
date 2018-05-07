9bd25bfa88aa22d926c16532b8da77d232144178
ge01 <gabriel.escobedo01@utrgv.edu>
2018-05-07 05:16:38 -0500

    Carpet Calculator App

---
+++ HCC/COSC_1436_CSharp/Chapter_04/Carpet/Carpet/CarpetCalculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpet
+{
+    class CarpetCalculator
+    {
+        private double pricePerSqYard;
+        private double noOfSqYards;
+
+        // Property of the pricePerSqYard data field
+        public double PricePerSqYard
+        {
+            get
+            {
+                return pricePerSqYard;
+            }
+            set
+            {
+                pricePerSqYard = value;
+            }
+        }
+
+        // Default constructor
+        public CarpetCalculator()
+        {
+            // empty body
+        }
+
+        // One argument constructor
+        public CarpetCalculator(double price)
+        {
+            PricePerSqYard = price;
+        }
+
+        // Two argument constructor
+        public CarpetCalculator(double amountNeeded, double price)
+        {
+            noOfSqYards = amountNeeded;
+            pricePerSqYard = price;
+        }
+
+        public double DetermineTotalCost()
+        {
+            return (pricePerSqYard * noOfSqYards);
+        }
+
+        // One of the overloaded mutator methods
+        public void SetNoOfSqYards(double length, double width)
+        {
+            const int SQ_FT_PER_SQ_YARD = 9;
+            noOfSqYards = length * width / SQ_FT_PER_SQ_YARD;
+        }
+
+        // One of the overloaded mutator methods
+        public void SetNoOfSqYards(double squareYards)
+        {
+            noOfSqYards = squareYards;
+        }
+
+        // Accessor method
+        public double GetNoOfSqYards()
+        {
+            return noOfSqYards;
+        }
+        public override string ToString()
+        {
+            return "Price Per Square Yard: " +
+                pricePerSqYard.ToString("C") +
+                "\nTotal square Yards: " +
+                noOfSqYards.ToString("F1") +
+                "\nTotal Price: " +
+                DetermineTotalCost().ToString("C");
+        }
+    }
+}
---
+++ HCC/COSC_1436_CSharp/Chapter_04/Carpet/Carpet/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carpet
+{
+    class CarpetCalculatorApp
+    {
+        static void Main(string[] args)
+        {
+            CarpetCalculator berber = new CarpetCalculator(17.95);
+
+            double roomWidth;
+            double roomLength;
+
+            DisplayInstructions();
+
+            // Call GetDimension() to get the length
+            roomLength = GetDimension("Length");
+
+            // Call GetDimension() again to get the width
+            roomWidth = GetDimension("Width");
+            berber.SetNoOfSqYards(roomLength, roomWidth);
+            berber.PricePerSqYard = 25.99;
+            Console.Clear();
+            Console.Write(berber);
+            Console.ReadLine();
+        }
+
+        static void DisplayInstructions()
+        {
+            Console.WriteLine("This program will"
+                                + " determine how much "
+                                + "carpet to purchase.");
+            Console.WriteLine();
+            Console.WriteLine("You will be asked to "
+                                + "enter the size of the "
+                                + "room and the price of the");
+            Console.WriteLine("carpet, in price per"
+                                + "square yds.");
+            Console.WriteLine();
+        }
+
+        static double GetDimension(string side)
+        {
+            string inputValue;      // local variables
+            int feet,               // needed only by this 
+                inches;             // method
+
+            Console.Write("Enter the {0} in feet: ",
+                            side);
+            inputValue = Console.ReadLine();
+            feet = int.Parse(inputValue);
+            Console.Write("Enter the {0} in inches: ",
+                            side);
+
+            inputValue = Console.ReadLine();
+            inches = int.Parse(inputValue);
+
+            // Note: cast required to avoid int division
+            return (feet + (double)inches / 12);
+        }
+    }
+}
