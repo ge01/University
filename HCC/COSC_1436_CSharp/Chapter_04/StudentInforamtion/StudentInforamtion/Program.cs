4bc791bb379d5416ebea95b8d8ab9720bcc07076
ge01 <gabriel.escobedo01@utrgv.edu>
2018-05-08 17:59:40 -0500

    Student Information App

---
+++ HCC/COSC_1436_CSharp/Chapter_04/StudentInforamtion/StudentInforamtion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInforamtion
+{
+    class StudentApp
+    {
+        static void Main(string[] args)
+        {
+            StudentInfo student1 = new StudentInfo("S12375896", "Gardanelli", 
+                                                   "Escondido", "Sophmore",
+                                                   "Computer Engineering");
+
+            student1.StudentTuition = GetTuition();
+            string sN = student1.StudentNumber;
+            string sC = student1.StudentClassification;
+            string sM = student1.StudentMajor;
+
+            Console.WriteLine();
+            Console.WriteLine("Student Number: " + sN);
+            Console.WriteLine("Students Full Name: " + student1.FirstAndLast());            
+            Console.WriteLine("Student Number: " + sC);
+            Console.WriteLine("Student Number: " + sM);
+            Console.WriteLine("Total Cost: {0:C}", student1.StudentTuition);
+            Console.WriteLine();
+        }
+
+        public static double GetTuition()
+        {
+            double cost;
+            double taxes = 0.08;
+            string inValue;
+
+            Console.Write("Student Tuition: ");
+            inValue = Console.ReadLine();
+            cost = double.Parse(inValue);
+
+            return (cost * taxes + cost);
+        }
+    }
+}
---
+++ HCC/COSC_1436_CSharp/Chapter_04/StudentInforamtion/StudentInforamtion/StudentInfo.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInforamtion
+{
+    class StudentInfo
+    {
+        private string studentNumber;
+        private string studentFirstName;
+        private string studentLastName;
+        private string studentClassification;
+        private string studentMajor;
+        private double studentTuition;
+
+        // Read-only property
+        public string StudentNumber
+        {
+            get
+            {
+                return studentNumber;
+            }
+        }
+
+        // Read-only property
+        public string StudentFirstName
+        {
+            get
+            {
+                return studentFirstName;
+            }
+        }
+
+        // Read-only property
+        public string StudentLastName
+        {
+            get
+            {
+                return studentLastName;
+            }
+        }
+
+        // Read-only property
+        public string StudentClassification
+        {
+            get
+            {
+                return studentClassification;
+            }
+        }
+
+        // Read-only property
+        public string StudentMajor
+        {
+            get
+            {
+                return studentMajor;
+            }
+        }
+
+        // Property acting as mutator and accessor
+        public double StudentTuition
+        {
+            set
+            {
+                studentTuition = value;
+            }
+            get
+            {
+                return studentTuition;
+            }
+        }
+
+        // Default constructor
+        public StudentInfo()
+        {
+        }
+
+        // Five parameter constructor
+        public StudentInfo(string sNumber, string firstName, 
+                           string lastName, string sClassification, 
+                           string sMajor)
+        {
+            studentNumber = sNumber;
+            studentFirstName = firstName;
+            studentLastName = lastName;
+            studentClassification = sClassification;
+            studentMajor = sMajor;
+        }
+
+        // Returns first and last name
+        public string FirstAndLast()
+        {
+            return studentFirstName + " " + studentLastName;
+        }
+    }
+}
